950615bef99c51b78c0f0c42e737b05cbdb2f3f7
ricardosantos9195-rgb <ricardosantos9195@gmail.com>
2026-09-10 18:56:13 +0200

    Adicionar sistema de heróis, castelos e Supabase

---
+++ app/src/main/assets/online-config.js
@@ -0,0 +1,6 @@
+/* Configuração pública do Supabase. A anon key pode estar na aplicação;
+   nunca colocar aqui a service_role key. */
+window.IMPERIO_ONLINE_CONFIG = {
+  supabaseUrl: 'https://wozjepbbsqxbhdicmsqh.supabase.co',
+  anonKey: 'sb_publishable_-3i8xUvlUoW1t33Z33YQ0w_IrHRsU7m'
+};
